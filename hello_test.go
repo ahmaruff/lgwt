964781bedc12f31284e01c78e0a514a1ec499dd0
Ahmad Ma'ruf <ahmadmaruf2701@gmail.com>
2025-03-17 11:27:21 +0700

    feat: update return value when empty string is provided

--- hello_test.go
+++ hello_test.go
@@ -3,10 +3,28 @@ package main
 import "testing"
 
 func TestHello(t *testing.T){
-    got := Hello("Chris")
-    want := "Hello, Chris"
+    t.Run("Saying Hello to people", func(t *testing.T) {
+        got := Hello("Chris")
+        want := "Hello, Chris"
 
-    if got != want {
+        if got != want {
+            t.Errorf("get %q want %q", got, want)
+        }
+    })
+
+    t.Run("Saying 'Hello, World' when empty string is provided", func(t *testing.T) {
+        got := Hello("")
+        want := "Hello, World"
+
+        if got != want {
+            t.Errorf("get %q want %q", got, want)
+        }
+    })
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+    t.Helper()
+    if(got != want) {    
         t.Errorf("get %q want %q", got, want)
     }
 }
