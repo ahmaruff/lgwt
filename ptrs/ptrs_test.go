51f85bc6f032a84ab5baf9c12522f57730f762a1
Ahmad Ma'ruf <ahmadmaruf2701@gmail.com>
2025-03-23 22:01:25 +0700

    feat: add withdraw method

--- ptrs/ptrs_test.go
+++ ptrs/ptrs_test.go
@@ -1,7 +1,6 @@
 package ptrs
 
 import (
-	"fmt"
 	"testing"
 )
 
@@ -14,9 +13,36 @@ func TestWallet(t *testing.T) {
 
     got := wallet.Balance()
 
-    var want Bitcoin = 1
+    var want Bitcoin = 10
 
     if got != want {
 		t.Errorf("got %s want %s", got, want)
 	}
+
+
+    t.Run("deposit", func(t *testing.T) {
+        wallet := Wallet{}
+        var btc Bitcoin = 10
+
+        wallet.Deposit(btc)
+        got := wallet.Balance()
+        var want Bitcoin = 10
+
+        if got != want {
+            t.Errorf("got %s want %s", got, want)
+        }
+    })
+
+    t.Run("withdraw", func(t *testing.T) {
+        wallet := Wallet{balance: Bitcoin(10)}
+
+        wallet.Withdraw(Bitcoin(5))
+        got := wallet.Balance()
+        want := Bitcoin(5)
+        
+
+        if got != want {
+            t.Errorf("got %s want %s", got, want)
+        }
+    })
 }
