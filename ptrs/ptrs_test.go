fa3b35ec7fe452436d8ffa7286be58f4d2507ab5
Ahmad Ma'ruf <ahmadmaruf2701@gmail.com>
2025-03-24 01:01:43 +0700

    fix: add error handling

--- ptrs/ptrs_test.go
+++ ptrs/ptrs_test.go
@@ -16,8 +16,9 @@ func TestWallet(t *testing.T) {
 
     t.Run("withdraw", func(t *testing.T) {
         wallet := Wallet{balance: Bitcoin(10)}
-        wallet.Withdraw(Bitcoin(5))
+        err := wallet.Withdraw(Bitcoin(5))
 
+        assertNoError(t, err)
         assertBalance(t, wallet, Bitcoin(5))
     })
 
@@ -54,4 +55,12 @@ func assertError(t testing.TB, got error, want error) {
     if got != want {
         t.Errorf("got %q, want %q", got, want)
     }
+}
+
+
+func assertNoError(t testing.TB, got error) {
+	t.Helper()
+	if got != nil {
+		t.Fatal("got an error but didn't want one")
+	}
 }
