e3d00b48b89538376377fd95b957717f330351eb
Ahmad Ma'ruf <ahmadmaruf2701@gmail.com>
2025-03-23 20:54:23 +0700

    feat: using struct as parameter

---
+++ struct/struct_test.go
@@ -0,0 +1,24 @@
+package structs
+
+import "testing"
+
+func TestPerimeter(t *testing.T) {
+    rect := Rectangle{10.0, 10.0}
+    got := Perimeter(rect)
+    want := 40.0
+
+    if got != want {
+        t.Errorf("got %.2f want %.2f", got, want)
+    }
+}
+
+
+func TestArea(t *testing.T) {
+    rect := Rectangle{10.0, 4.0}
+    got := Area(rect)
+    want := 40.0
+
+    if got != want {
+        t.Errorf("got %.2f want %.2f", got, want)
+    }
+}
