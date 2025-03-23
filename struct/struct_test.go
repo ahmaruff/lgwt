4121e695759eb4c91ce2142056fa92f8ae7dce4a
Ahmad Ma'ruf <ahmadmaruf2701@gmail.com>
2025-03-23 21:19:50 +0700

    refactor using table driven test

--- struct/struct_test.go
+++ struct/struct_test.go
@@ -2,6 +2,11 @@ package structs
 
 import "testing"
 
+
+type Shape interface {
+    Area() float64
+}
+
 func TestPerimeter(t *testing.T) {
     rect := Rectangle{10.0, 10.0}
     got := Perimeter(rect)
@@ -14,11 +19,23 @@ func TestPerimeter(t *testing.T) {
 
 
 func TestArea(t *testing.T) {
-    rect := Rectangle{10.0, 4.0}
-    got := Area(rect)
-    want := 40.0
 
-    if got != want {
-        t.Errorf("got %.2f want %.2f", got, want)
+
+    areaTest := []struct {
+        s Shape
+        want float64
+    }{
+        {Rectangle{10.0, 4.0}, 40.0},
+        {Circle{10.0}, 314.1592653589793},
+    }
+
+
+    for _, test := range(areaTest) {
+        got := test.s.Area()
+
+        if got != test.want {
+			t.Errorf("got %g want %g", got, test.want)
+        }
+
     }
 }
